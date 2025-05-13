ed3db74c9e9f486089527379ea44d50ff3feedd3
ALIVAN7 <alivan7@hotmail.com>
2025-05-12 22:20:58 -0300

    add: method getCreinfo

--- src/main/java/edu/alenasoft/principlespatterns/patterns/iterator/CREUser.java
+++ src/main/java/edu/alenasoft/principlespatterns/patterns/iterator/CREUser.java
@@ -1,6 +1,16 @@
 package edu.alenasoft.principlespatterns.patterns.iterator;
 
+import java.util.*;
+
 public class CREUser {
     private int id;
     private UserData userData;
+
+    public  Map<Integer, UserData> getCREinfo() {
+        Map<Integer,UserData> mapUser = new HashMap<>();
+            mapUser.put(1,new UserData("usuario1","direccion1",20.0));
+            mapUser.put(2,new UserData("usuario2","direccion2",10.5));
+            mapUser.put(3,new UserData("usuario3","direccion3",10.5));
+        return mapUser;
+    }
 }
--- src/main/java/edu/alenasoft/principlespatterns/patterns/iterator/UserData.java
+++ src/main/java/edu/alenasoft/principlespatterns/patterns/iterator/UserData.java
@@ -4,4 +4,36 @@ public class UserData {
 private String name;
 private String address;
 private double payment;
+
+    public UserData(String name, String address, double payment) {
+        this.name = name;
+        this.address = address;
+        this.payment = payment;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getPayment() {
+        return payment;
+    }
+
+    public void setPayment(double payment) {
+        this.payment = payment;
+    }
+
+
 }
