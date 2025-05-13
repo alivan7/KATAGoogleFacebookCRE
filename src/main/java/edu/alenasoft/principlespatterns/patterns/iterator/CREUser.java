cd34a1e0e6d0e23865707fa6bf40327b9b296dc4
ALIVAN7 <alivan7@hotmail.com>
2025-05-12 21:59:34 -0300

    add:userData and CREUser added

---
+++ src/main/java/edu/alenasoft/principlespatterns/patterns/iterator/CREUser.java
@@ -0,0 +1,6 @@
+package edu.alenasoft.principlespatterns.patterns.iterator;
+
+public class CREUser {
+    private int id;
+    private UserData userData;
+}
---
+++ src/main/java/edu/alenasoft/principlespatterns/patterns/iterator/UserData.java
@@ -0,0 +1,7 @@
+package edu.alenasoft.principlespatterns.patterns.iterator;
+
+public class UserData {
+private String name;
+private String address;
+private double payment;
+}
